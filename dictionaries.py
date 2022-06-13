3918ebaa443c465316e8d8d68b00dba42629a0b3
BibekMhrz <mhrz.bibek@gmail.com>
2022-06-13 14:25:41 +0545

    dictionaries and functions are added

---
+++ dictionaries.py
@@ -0,0 +1,8 @@
+locations={"Ram":"Lalitpur", "Shyam":"Kathmandu", "Hari":"Bhaktapur"}
+#here locations act as dictionaries
+print(locations["Ram"])  #dictionaries takes the value of Ram
+
+locations["Krishna"]="Lalitpur"
+print(locations)
+
+
